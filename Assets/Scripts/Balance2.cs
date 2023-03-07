efd52cb95cf1e5888a45441bb914106c6a8f5e00
LauJz <122171445+LauJz@users.noreply.github.com>
2023-03-08 00:11:47 +0100

    Ragdoll movement  + jump + flip

---
+++ Assets/Scripts/Balance2.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balance2 : MonoBehaviour
+{
+    public float targetRotation;
+    Rigidbody2D rb;
+    public float force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       rb.MoveRotation(Mathf.LerpAngle(rb.rotation, targetRotation, force * Time.deltaTime)); 
+    }
+}
