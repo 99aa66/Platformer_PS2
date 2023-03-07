31efcf8fb61fd430d03ab19625b90183fdc6073b
LauJz <122171445+LauJz@users.noreply.github.com>
2023-03-07 19:31:24 +0100

    Physics + grap

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator anim;
+    public Rigidbody2D rb;
+    public float jumpForce;
+    public float playerSpeed;
+    public Vector2 jumpHeight;
+    private bool isOnGround;
+    public float positionRadius;
+    public LayerMask ground;
+    public Transform playerPos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Collider2D[] colliders = transform.GetComponentsInChildren<Collider2D>();
+        for (int i = 0; i < colliders.Length; i ++)
+        {
+            for (int k = i; k < colliders.Length; k++)
+            {
+                Physics2D.IgnoreCollision(colliders[i], colliders[k]);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxisRaw("Horizontal") != 0)
+        {
+            if(Input.GetAxisRaw("Horizontal") > 0)
+            {
+                anim.Play("Walk");
+                rb.AddForce(Vector2.right * playerSpeed * Time.deltaTime);
+            }
+            else
+            {
+                anim.Play("WalkBack");
+                rb.AddForce(Vector2.left * playerSpeed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            anim.Play("Idle");
+        }
+
+        isOnGround = Physics2D.OverlapCircle(playerPos.position, positionRadius, ground);
+        if(isOnGround == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("jumping");
+            rb.AddForce(Vector2.up * jumpForce * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/arms.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arms : MonoBehaviour
+{
+    int speed = 300;
+    public Rigidbody2D rb;
+    public Camera cam;
+    public KeyCode mousebutton;
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerpos = new Vector3(cam.ScreenToWorldPoint(Input.mousePosition).x, cam.ScreenToWorldPoint(Input.mousePosition).y, 0);
+        Vector3 difference = playerpos - transform.position;
+        float rotationZ = Mathf.Atan2(difference.x, difference.y) * Mathf.Rad2Deg;
+        if (Input.GetKey(mousebutton))
+        {
+            rb.MoveRotation(Mathf.LerpAngle(rb.rotation, rotationZ, speed * Time.fixedDeltaTime));
+        }
+    }
+}
