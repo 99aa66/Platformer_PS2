46ae36aefa87933435ee021f40f67778144c55a2
LauJz <122171445+LauJz@users.noreply.github.com>
2023-03-12 17:43:29 +0100

    correct name camera

--- Assets/Scripts/V1/CameraManager.cs
+++ Assets/Scripts/V1/CameraManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraManager : MonoBehaviour
+public class CameraManagerperdu : MonoBehaviour
 {
     [SerializeField] GameObject playerRef;
     Vector3 refVelocity = Vector3.zero;
