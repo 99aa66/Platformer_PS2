01a344681757e8243e6052c277d82164f7df8317
RBasic <rcham.pro@gmail.com>
2023-04-05 13:23:53 +0200

    new ZITO + Respawn

--- Assets/Scripts/v8/DeathZone.cs
+++ Assets/Scripts/v8/DeathZone.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 using System.Collections;
+using Unity.VisualScripting;
 
 public class DeathZone : MonoBehaviour
 {
     private Transform playerSpawn;
     private Animator fadeSystem;
+    private bool triggered = false;
     private void Awake()
     {
         playerSpawn = GameObject.FindGameObjectWithTag("PlayerSpawn").transform;
@@ -15,14 +17,33 @@ public class DeathZone : MonoBehaviour
     {
         if(collision.CompareTag("Player")) //vérifier si c'est le joueur qui est entré en contact dans la zone, collision = player
         {
-            StartCoroutine(ReplacePlayer(collision));
+            if (!triggered)
+            {
+                triggered = true;
+                Rigidbody2D[] rigidbodies = collision.transform.root.GetComponentsInChildren<Rigidbody2D>();
+                Rigidbody2D hancheRef = null;
+                for (int i = 0; i < rigidbodies.Length; i++)
+                {
+                    if (rigidbodies[i].gameObject.name == "Hanche")
+                    {
+                        hancheRef = rigidbodies[i];
+                        break;
+                    }
+                }
+                hancheRef.isKinematic = true;
+                hancheRef.velocity = Vector3.zero;
+                GameObject playerRef = collision.transform.root.gameObject;
+                StartCoroutine(ReplacePlayer(hancheRef));
+            }
         }
     }
 
-    private IEnumerator ReplacePlayer(Collider2D collision)
+    private IEnumerator ReplacePlayer(Rigidbody2D hancheRef)
     {
         fadeSystem.SetTrigger("FadeIn");
         yield return new WaitForSeconds(1f);
-        collision.transform.position = playerSpawn.position; //position objet collision replacer au position du PlayerSpawn
+        hancheRef.transform.position = playerSpawn.position; //position objet collision replacer au position du PlayerSpawn
+        hancheRef.isKinematic = false;
+        triggered = false;
     }
 }
