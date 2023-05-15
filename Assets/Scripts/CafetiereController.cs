33c15bb8e8282bffb7ab38e1aeff00adcbffea66
LauJz <122171445+LauJz@users.noreply.github.com>
2023-05-15 04:03:24 +0200

    autoConfigureConnectedAnchor désactivé pour cafetière

--- Assets/Scripts/CafetiereController.cs
+++ Assets/Scripts/CafetiereController.cs
@@ -6,7 +6,6 @@ public class CafetiereController : MonoBehaviour
 {
     public int durability = 5;
     public bool isBroken { get { return durability <= 0; } }
-    public bool isBeingHeld;
     private Vector3 initialPosition;
     private Quaternion initialRotation;
 
@@ -16,7 +15,6 @@ public class CafetiereController : MonoBehaviour
     private const float transparentAlpha = 0.5f;
 
     public Grabbing grabbingScript;
-
     public Animator anim;
     void Awake()
     {
@@ -35,15 +33,31 @@ public class CafetiereController : MonoBehaviour
     public void DecrementDurability()
     {
         durability--;
-        if (isBroken)
+
+        switch (durability)
         {
-            anim.SetTrigger("break");
-            sr.color = new Color(1f, 1f, 1f, transparentAlpha);
-            anim.SetTrigger("ResetPosition");
-            StartCoroutine(Static());
-            ResetPosition();
+            case 4:
+                sr.color = Color.yellow;
+                break;
+            case 3:
+                sr.color = new Color(1f, 0.5f, 0f);
+                break;
+            case 2:
+                sr.color = new Color(1f, 0.3f, 0f);
+                break;
+            case 1:
+                sr.color = Color.red;
+                break;
+            default:
+                anim.SetTrigger("break");
+                sr.color = new Color(1f, 1f, 1f, transparentAlpha);
+                anim.SetTrigger("ResetPosition");
+                StartCoroutine(Static());
+                ResetPosition();
+                break;
         }
     }
+
     public void ResetPosition()
     {
         transform.position = initialPosition;
@@ -60,26 +74,18 @@ public class CafetiereController : MonoBehaviour
         if (col.gameObject.CompareTag("Ennemi") || col.gameObject.CompareTag("Glass") || col.gameObject.CompareTag("Ground"))
         {
             DecrementDurability();
-            if (grabbingScript != null)
+            if (isBroken)
             {
                 grabbingScript.DestroyJoint();
             }
         }
-        /*else if (col.gameObject.CompareTag("Ground") && !isHoldingObject && col.relativeVelocity.magnitude > 50f)
-        {
-            isHoldingObject = false;
-            anim.SetTrigger("break");
-            ResetPosition();
-        }*/
     }
-
     private IEnumerator Static()
     {
+        //yield return new WaitForSeconds(0.5f);
         rb.bodyType = RigidbodyType2D.Static;
-        //rb.simulated = false;
-        // Attendre 0,5 seconde pour que l'objet s'immobilise complètement
+        // Attendre 1 seconde pour que l'objet s'immobilise complètement
         yield return new WaitForSeconds(1f);
-        //rb.simulated = true;
         // Réaffecter le rb par défaut à l'objet
         rb.bodyType = RigidbodyType2D.Dynamic;
     }
--- Assets/Scripts/Grabbing.cs
+++ Assets/Scripts/Grabbing.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 
 public class Grabbing : MonoBehaviour
 {
-    public Rigidbody2D head;
+    [SerializeField] private Rigidbody2D head;
     public KeyCode mouseButton;
     private bool canGrab;
     private FixedJoint2D joint;
 
     private GameObject currentlyHolding;
     private bool isHoldingObject;
+    private CafetiereController cafControl;
 
+    private void Start()
+    {
+        cafControl = GetComponent<CafetiereController>();
+    }
     void Update()
     {
         if (Input.GetKeyDown(mouseButton) && !isHoldingObject)
         {
             canGrab = true;
+
         }
         if (Input.GetKeyUp(mouseButton))
         {
@@ -42,75 +48,56 @@ public class Grabbing : MonoBehaviour
             currentlyHolding = null;
             isHoldingObject = false;
         }
-        // Vérifier si la cafetière est cassée
-        /*if (currentlyHolding != null && currentlyHolding.CompareTag("Cafetière"))
-        {
-            CafetiereController cafetiereController = currentlyHolding.GetComponent<CafetiereController>();
-            if (cafetiereController == null || cafetiereController.durability <= 0)
-            {
-                if (joint != null)
-                {
-                    Destroy(joint);
-                    joint = null;
-                }
-                isHoldingObject = false;
-                currentlyHolding = null;
-            }
-        }*/
     }
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (canGrab && !isHoldingObject)
+        cafControl = col.gameObject.GetComponent<CafetiereController>();
+
+        if (!canGrab || isHoldingObject)
         {
-            if (col.CompareTag("Cafetière"))
+            return;
+        }
+
+        if (col.CompareTag("Cafetière"))
+        {
+            currentlyHolding = col.gameObject;
+            isHoldingObject = true;
+
+            FixedJoint2D[] joints = currentlyHolding.GetComponents<FixedJoint2D>();
+            bool alreadyConnected = false;
+
+            foreach (FixedJoint2D j in joints)
             {
-                currentlyHolding = col.gameObject;
-                isHoldingObject = true;
-
-                FixedJoint2D[] joints = currentlyHolding.GetComponents<FixedJoint2D>();
-                bool alreadyConnected = false;
-                for (int i = 0; i < joints.Length; i++)
+                if (j.connectedBody == head)
                 {
-                    if (joints[i].connectedBody == head)
-                    {
-                        alreadyConnected = true;
-                        break;
-                    }
-                }
-
-                if (!alreadyConnected)
-                {
-                    joint = currentlyHolding.AddComponent<FixedJoint2D>();
-                    joint.connectedBody = head;
-                }
-
-                Rigidbody2D rb = currentlyHolding.GetComponent<Rigidbody2D>();
-                if (rb != null && rb.isKinematic)
-                {
-                    rb.isKinematic = false;
-                }
-
-                CafetiereController cafetiereController = currentlyHolding.GetComponent<CafetiereController>();
-                if (cafetiereController.gameObject==null || cafetiereController.durability <= 0)
-                {
-                    if (joint != null)
-                    {
-                        Destroy(joint);
-                        joint = null;
-                    }
-                    isHoldingObject = false;
-                    currentlyHolding = null;
+                    alreadyConnected = true;
+                    break;
                 }
             }
-            else if (col.CompareTag("Player") || col.CompareTag("BossMama") || col.CompareTag("BossLasagne"))
+
+            if (!alreadyConnected)
             {
-                // Do nothing
+                joint = currentlyHolding.AddComponent<FixedJoint2D>();
+                joint.connectedBody = head;
+                joint.autoConfigureConnectedAnchor = false;
             }
-            else
+
+            Rigidbody2D rb = currentlyHolding.GetComponent<Rigidbody2D>();
+
+            if (rb != null && rb.isKinematic)
             {
-                // ne rien faire si l'objet ne correspond à aucune condition
-                return;
+                rb.isKinematic = false;
             }
+
+            else if ((cafControl.gameObject != null || cafControl.durability <= 0) && alreadyConnected)
+            {
+                DestroyJoint();
+            }
+        }
+
+        else if (col.CompareTag("Player") || col.CompareTag("BossMama") || col.CompareTag("BossLasagne"))
+        {
+            return;
         }
     }
     public void DestroyJoint()
@@ -120,5 +107,7 @@ public class Grabbing : MonoBehaviour
             Destroy(joint);
             joint = null;
         }
+        isHoldingObject = false;  // on indique que l'on ne tient plus d'objet
+        currentlyHolding = null;
     }
 }
